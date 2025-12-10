af7627e3904e807347ddc4dc91de2c2bddc0a6bd
Henrique De Vasconcelos <41359575+rikenze@users.noreply.github.com>
2025-12-10 20:49:29 +0100

    added environment variable for swagger usage

--- FalconTouch.Api/Program.cs
+++ FalconTouch.Api/Program.cs
@@ -151,7 +151,7 @@ using (var scope = app.Services.CreateScope())
     }
 }
 
-if (app.Environment.IsDevelopment())
+if (config.GetValue<bool>("Swagger:Enabled"))
 {
     app.UseSwagger();
     app.UseSwaggerUI();
